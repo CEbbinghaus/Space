aeb7b59bdfe9818f2d5f9e6f4d36923e7ae77ba5
Unknown <jecebbi@gmail.com>
2018-10-12 01:12:07 +1100

    Now More Support and First Proper Version.

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 class Space{
-  constructor(e, f = false){
+  constructor(e, f = false, c = false){
     this.canvas = e || document.getElementById(e) || document.getElementsByTagName("canvas");
     this.ctx = this.canvas.getContext("2d");
     this.cr = this.canvas.getClientRects()[0];
+    this.center = c;
     this.width = 0;
     this.height = 0;
-    this.stars = Array.from(new Array(100));
+    this.stars = Array.from(new Array((this.cr.width + this.cr.height) / 20 | 0));
     this.init();
     this.next = null;
     this.xo = 0;
     this.yo = 0;
     this.f = f;
+    this.r = 5;
     this.x = 0;
     this.y = 0;
   }
   init(){
     this.stars.map((e, i) => {this.stars[i] = new Star()})
-    if(this.f){
-      this.resize();
-      onresize = this.resize.bind(this);
-    }else{
-      this.canvas.width = this.width = this.cr.width;
-      this.canvas.height = this.height = this.cr.height;  
-    }
+    this.resize();
+    onresize = this.resize.bind(this);
     onmousemove = this.move.bind(this)
     this.next = requestAnimationFrame(this.update.bind(this))
   }
   resize(){
-    this.canvas.width = this.width = innerWidth;
-    this.canvas.height = this.height = innerHeight;
+    this.cr = this.canvas.getClientRects()[0];
+    if(this.f){
+      this.canvas.width = this.width = innerWidth;
+      this.canvas.height = this.height = innerHeight;
+    } else {
+      this.canvas.width = this.width = this.cr.width;
+      this.canvas.height = this.height = this.cr.height;
+    }
   }
   move(e){
     let evt = e || event;
@@ -36,8 +39,10 @@ class Space{
     let y = e.clientY - this.cr.top;
     // if (x < 0 || y < 0 || x > this.width || y > this.width)return;
     let d = (innerWidth + innerHeight) / 1000
-    this.xo = (this.width / 2 - x) / d;
-    this.yo = (this.height / 2 - y) / d;
+    this.xo = ((this.center ? innerWidth : this.width) / 2 - (this.center ? evt.clientX : x)).clamp(this.width / 2);
+    this.yo = ((this.center ? innerHeight : this.height) / 2 - (this.center ? evt.clientY : y)).clamp(this.height / 2);
+    // this.xo = ((this.center ? innerWidth : this.width) / 2 - this.center ? e.clientX : x) / d;
+    // this.yo = ((this.center ? innerHeight : this.height) / 2 - this.center ? e.clientY : y) / d;
   }
   update(){
     let d = 10;
@@ -57,7 +62,7 @@ class Space{
     this.ctx.fillStyle = "#ccc";
     this.ctx.clearRect(0, 0, this.width, this.height)
     this.stars.forEach(s => {
-      s.draw(this.ctx);
+      s.draw(this.ctx, this.r);
     })
   }
 }
@@ -67,9 +72,14 @@ class Star{
     this.y = Math.random() * innerHeight;
     this.z = Math.random();
   }
-  draw(ctx){
+  draw(ctx, r){
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.z * 5, 0, 2*Math.PI);
+    ctx.arc(this.x, this.y, this.z * r, 0, 2*Math.PI);
     ctx.fill();
   }
+}
+Number.prototype.clamp = function (n) {
+  if(this < -n)return -n;
+  if(this > n)return n;
+  return this;
 }
