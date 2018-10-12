85973d1a586255dc5bef9cc63dc4eff573f1ea11
Tom Burris <unsaneity@gmail.com>
2018-10-11 21:09:30 -0700

    improved star placement on resize

--- space.js
+++ space.js
@@ -1,74 +1,78 @@
+"use strict";
+
 class Space{
   constructor(e, f = false, c = true){
-    this.canvas = e.localName == 'canvas' ? e : document.getElementById(e);
+    this.canvas = e.localName == "canvas" ? e : document.getElementById(e);
     if(!this.canvas)throw("You must Provide a Canvas Element");
-    this.ctx = this.canvas.getContext("2d");
-    this.cr = this.canvas.getClientRects()[0];
     this.center = c;
-    this.width = 0;
-    this.height = 0;
-    this.stars = Array.from(new Array((this.cr.width + this.cr.height) / 20 | 0));
-    this.next = null;
+    this.f = f;
+    this.ctx = this.canvas.getContext("2d");
+    this.cr = null; // canvas element bounding rectangle
+    this.stars = [];
     this.xo = 0;
     this.yo = 0;
-    this.f = f;
     this.r = 5;
     this.x = 0;
     this.y = 0;
-    this.init();
-  }
-  init(){
-    this.stars.map((e, i) => {this.stars[i] = new Star()})
-    onresize = this.resize.bind(this);
-    onmousemove = this.move.bind(this)
-    this.next = requestAnimationFrame(this.update.bind(this))
     this.resize();
+    this.move({clientX: innerWidth / 4, clientY: innerHeight / 4});
+    addEventListener("resize", this.resize = this.resize.bind(this));
+    addEventListener("mousemove", this.move = this.move.bind(this));
+    requestAnimationFrame(this.update = this.update.bind(this));
   }
   resize(){
-    this.cr = this.canvas.getClientRects()[0];
+    const oldW = this.canvas.width;
+    const oldH = this.canvas.height;
     if(this.f){
-      this.canvas.width = this.width = innerWidth;
-      this.canvas.height = this.height = innerHeight;
-    } else {
-      this.canvas.width = this.width = this.cr.width;
-      this.canvas.height = this.height = this.cr.height;
+      this.canvas.width = innerWidth;
+      this.canvas.height = innerHeight;
     }
+    this.cr = this.canvas.getClientRects()[0];
+    this.updateStars(oldW, oldH);
   }
   move(e){
-    let evt = e || event;
-    let x = e.clientX - this.cr.left;
-    let y = e.clientY - this.cr.top;
-    // if (x < 0 || y < 0 || x > this.width || y > this.width)return;
-    let d = (innerWidth + innerHeight) / 1000
-    this.xo = ((!this.center ? innerWidth : this.width) / 2 - (!this.center ? evt.clientX : x)).clamp(this.width / 2);
-    this.yo = ((!this.center ? innerHeight : this.height) / 2 - (!this.center ? evt.clientY : y)).clamp(this.height / 2);
+    const x = e.clientX - this.cr.left;
+    const y = e.clientY - this.cr.top;
+    const w = this.canvas.width;
+    const h = this.canvas.height;
+    this.xo = ((!this.center ? innerWidth : w) / 2 - x).clamp(w / 2);
+    this.yo = ((!this.center ? innerHeight : h) / 2 - y).clamp(h / 2);
+  }
+  updateStars(oldW, oldH){
+    const xScaler = this.canvas.width / oldW;
+    const yScaler = this.canvas.height / oldH;
+    const target = (this.canvas.width * this.canvas.height) / 1e4 | 0;
+    if (target < this.stars.length) this.stars.length = target;
+    for (let s of this.stars) {
+      s.x *= xScaler;
+      s.y *= yScaler;
+    }
+    const extra = target - this.stars.length;
+    for(let i = 0; i < extra; ++i){
+      this.stars.push(new Star(this.canvas.width, this.canvas.height));
+    }
   }
   update(){
     let d = 10;
-    this.stars.map(s => {
-      s.x += s.z * (this.xo / (innerWidth / 50));
-      s.y += s.z * (this.yo / (innerHeight / 50));
-      s.x %= this.width;
-      if(s.x<0)s.x+=this.width;
-      s.y %= this.height;
-      if(s.y<0)s.y+=this.height;
-      return s;
-    })
+    const w = this.canvas.width;
+    const h = this.canvas.height;
+    for(let s of this.stars){
+      s.x = (s.x + s.z * (this.xo / (innerWidth / 50)) + w) % w;
+      s.y = (s.y + s.z * (this.yo / (innerHeight / 50)) + h) % h;
+    }
     this.draw();
-    this.next = requestAnimationFrame(this.update.bind(this));
+    requestAnimationFrame(this.update);
   }
   draw(){
     this.ctx.fillStyle = "#ccc";
-    this.ctx.clearRect(0, 0, this.width, this.height)
-    this.stars.forEach(s => {
-      s.draw(this.ctx, this.r);
-    })
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    for (const s of this.stars) s.draw(this.ctx, this.r);
   }
 }
 class Star{
-  constructor(){
-    this.x = Math.random() * innerWidth;
-    this.y = Math.random() * innerHeight;
+  constructor(w, h){
+    this.x = Math.random() * w;
+    this.y = Math.random() * h;
     this.z = Math.random();
   }
   draw(ctx, r){
@@ -77,8 +81,8 @@ class Star{
     ctx.fill();
   }
 }
-Number.prototype.clamp = function (n) {
+Number.prototype.clamp = function(n){
   if(this < -n)return -n;
   if(this > n)return n;
   return this;
-}
+};
